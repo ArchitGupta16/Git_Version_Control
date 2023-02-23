cf43d0a446fe594f871cf8341576c397a97a7735
ArchitGupta16 <gupta.archit2001@gmail.com>
2023-02-23 19:15:20 +0530

    Final Calculator

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,9 @@
+print("MENU\n1.Add\n2.Subtract\n3.Multiplication\n4.Divide\n5.Exit")
+choice = int(input("Enter your choice:"))
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+
 def add(a,b):
     return a+b
 def sub(a,b):
@@ -8,3 +14,18 @@ def mul(a,b):
 
 def div(a, b):
     return a/b
+
+cont = "Y"
+while choice in range(1,6) and cont=="Y":
+    if choice == 1:
+        print("Addition:",add(a,b))
+    elif choice == 2:
+        print("Subtraction:",sub(a,b))
+    elif choice == 3:
+        print("Multiplication",mul(a,b))
+    elif choice == 4:
+        print("Division",div(a,b))
+    else:
+        print("Invalid Choice!!")
+        exit()
+    cont=input("Do you want to continue?(Y/N):")
