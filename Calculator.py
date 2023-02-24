143c6af7dec69252d9f6f16c463fb4842a0146dc
Shubhi2807 <tshubhi2807@gmail.com>
2023-02-24 19:08:49 +0530

    V2 Final Calculator

--- Calculator.py
+++ Calculator.py
@@ -1,30 +1,25 @@
+from Subtraction import subtract
+from Multiplication import multi
+from Division import division
+from Add import add
+
 print("MENU\n1.Add\n2.Subtract\n3.Multiplication\n4.Divide\n5.Exit")
 choice = int(input("Enter your choice:"))
 
 a = int(input("Enter first number:"))
 b = int(input("Enter second number:"))
 
-def add(a,b):
-    return a+b
-def sub(a,b):
-    return (a-b)
 
-def mul(a,b):
-    return (a*b)
-
-def div(a, b):
-    return a/b
-
-cont = "Y"
-while choice in range(1,6) and cont=="Y":
+cont = "y"
+while choice in range(1,6) and cont.lower=="y":
     if choice == 1:
         print("Addition:",add(a,b))
     elif choice == 2:
-        print("Subtraction:",sub(a,b))
+        print("Subtraction:",subtract(a,b))
     elif choice == 3:
-        print("Multiplication",mul(a,b))
+        print("Multiplication", multi(a,b))
     elif choice == 4:
-        print("Division",div(a,b))
+        print("Division",division(a,b))
     else:
         print("Invalid Choice!!")
         exit()
