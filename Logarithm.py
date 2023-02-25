3fe1a383e36a9fa1b491685bce58150c8371635a
Yuvraj <yuvrajshekhawatpilani@gmail.com>
2023-02-25 14:51:49 +0530

    V2 Up_Sqrt & Log

--- Logarithm.py
+++ Logarithm.py
@@ -1,4 +1,8 @@
 import math
 
-def logarithm(a):
-    return math.log(a)
+def logarithm():
+    a = int(input("Enter number to calculate log : "))
+    if a > 0:
+        print(math.log(a))
+    else:
+        print("Invalid Input !!")
--- SquareRoot.py
+++ SquareRoot.py
@@ -1,2 +1,6 @@
-def squareRoot(a):
-    return a**(1/2)
+def squareRoot():
+    a = int(input("Enter number to get Square root : "))
+    if a > 0:
+        print(a**(1/2))
+    else:
+        print("Invalid Input !!")
